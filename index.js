713e25979982a9ac475c3a0f4a095cd9843e61a1
Beau Witka <bwitka@gmail.com>
2019-02-24 14:29:16 -0800

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
-}
-
-// Returns true if the string is empty or consists solely of whitespaces.
-String.prototype.blank = function(string) {
-  if (string === /^\s*$/) {
-    return true;
-  }
-}
-
-// Return last element of an array.
-Array.prototype.last = function(array) {
-  return array.slice(-1);
 }
 
 // Defines a Phrase object.
@@ -21,7 +11,14 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Phrase object (with palindrome detector)
+___ 
+
+This is a sample NPM module created in [Learn Enough JavaScript to Be Dangerous](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
